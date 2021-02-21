d56e777972e768434c79cbeb2c026b91510023c8
Emir Cogo <emir.cogo@eurobit.ba>
2021-02-21 02:24:13 +0100

    get lazy loading error fixex

--- src/main/java/ba/ecogo/grocerymonitor/repository/SectionRepository.java
+++ src/main/java/ba/ecogo/grocerymonitor/repository/SectionRepository.java
@@ -1,6 +1,17 @@
 package ba.ecogo.grocerymonitor.repository;
 
 import ba.ecogo.grocerymonitor.model.Section;
+import org.springframework.data.jpa.repository.EntityGraph;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface SectionRepository extends BaseModelRepository<Section>{
+    @Override
+    @EntityGraph(attributePaths = {"items.product"})
+    List<Section> findByUserId(UUID userId);
+
+    @Override
+    @EntityGraph(attributePaths = {"items.product"})
+    Section findByIdAndUserId(UUID id, UUID userId);
 }
